c80697834fd7a4272860fb38bb3cfe178b6c5be6
yutao.zhang <zyttt_o@163.com>
2024-04-30 16:54:54 +0800

    switch ppc updating 2024/4/30 16:54

--- ME/CommunityCanteenSys/Mysql_script/initDatabase_add_userPwdTable.sql
+++ ME/CommunityCanteenSys/Mysql_script/initDatabase_add_userPwdTable.sql
@@ -3,8 +3,9 @@ use GraduationProject_CommunityCanteenSys;
 
 create table loginInfo
 (
-	username varchar(10) not null unique primary key ,
-    pwd varchar(20) not null
+	username varchar(10),
+    pwd varchar(20),
+    curid varchar(20)
 );
  
-insert into loginInfo values('admin','admin');
+insert into loginInfo values('admin','admin','0');
